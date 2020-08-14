fc681483fdf93e9efc316e597ee59c200022cc70
Barak Roth <brk.rot@gmail.com>
2020-08-14 14:15:23 +0300

    Finished the AB-csv and the srt-script-csv for both movies

--- Barak.py
+++ Barak.py
@@ -2,11 +2,12 @@ import csv
 import pathlib
 
 
-def abCSV():
-    '************************************PART 1.D --> AB.CSV***************************************************'
-    scriptFile = "Dark Knight Rises.txt"
-    subtitlesFile = "Dark Knight Rises.srt"
-    text = open(scriptFile, encoding="utf8").read().splitlines()
+def makeABandScriptSrt(MovieName):
+    print(MovieName)
+    '************************************PART 1 AB.CSV***************************************************'
+    scriptFile = MovieName+".txt"
+    subtitlesFile = MovieName+".srt"
+    scriptText = open(scriptFile, encoding="utf8").read().splitlines()
     # Initilizg all the 3 lists
     response = []
     speakers = []
@@ -18,7 +19,7 @@ def abCSV():
                  '!', 'OMITTED', 'THE END', 'GASPS']
     # Stage 1 - finding the names of the speakers(and the line in the script where it happens)
     i = 0
-    for line in text:
+    for line in scriptText:
         line = line.strip()
         if (line.isupper()):
             # Only for cleaning the look of it
@@ -52,8 +53,8 @@ def abCSV():
         nextSpeaker = speakers[i + 1]
 
         # Getting all the lines of the speaker
-        while (not nextSpeaker in text[speakerText]):
-            line = text[speakerText].strip()
+        while (not nextSpeaker in scriptText[speakerText]):
+            line = scriptText[speakerText].strip()
             if 'INT' in line or '(CONTINUED)' in line or line.isdigit() or '(' in line or ' 3/1/02' in line or 'FOOTSTEPS' in line or 'DISSOLVE' in line:
                 break
             if 'EXT' in line or 'INSERT' in line or 'OPENS' in line or 'OMITTED' in line or 'CRASH!' in line or 'APPEARS' in line or 'LURCHES,' in line:
@@ -65,12 +66,11 @@ def abCSV():
 
         whatIsSaid.append(str)
         str = ""
-    # print("whatIsSaid")
-    # print(whatIsSaid)
+
     # export the table to a CSV file
     i = 0
     size = len(response)
-    with open('AB.csv', 'w') as csvfile:
+    with open(MovieName+' AB.csv', 'w',encoding='utf8') as csvfile:
         table_write = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         table_write.writerow(("Response", 'Speaker', 'What is said'))
         for line in range(0, size - 1):
@@ -78,6 +78,80 @@ def abCSV():
                 table_write.writerow((response[i], speakers[i], whatIsSaid[i]))
             i = i + 1
     '************************************PART D***************************************************'
+    EnglishSubtitles = open(subtitlesFile, encoding="utf8").read()
+    SubtitlesList = EnglishSubtitles.split('\n\n')
+    # Initlizing the lists
+    number = []
+    beginTime = []
+    endTime = []
+    scriptText = []
 
+    for singleSubtitle in SubtitlesList:
+        if (singleSubtitle == ''):
+            break
+        subtitleParts = singleSubtitle.split('\n')
+        # print(subtitleParts)
+        times = subtitleParts[1].split(' --> ')
+        number.append(subtitleParts[0])
+        str = ''
+        for i in range(2, len(subtitleParts)):
+            str += ' '
+            str += subtitleParts[i].replace('-', '')
+        scriptText.append(str)
+        beginTime.append(times[0])
+        endTime.append(times[1])
+    '************************************Merging***************************************************'
+    subtitles = scriptText
+    srtDictionary = {}
+    set = []
+    # Getting all the words that are "special" - have only one instance + their location in the subtitles order
+    # a part of the set is word, its last location said, number of times
+    # since we dont care for those words who has more than one time in the srt, the last location is good for us
+    for i in range(len(subtitles)):
+        words = subtitles[i].split(' ')     #A whole row to seperate words
+        for word in words:
+            # Cleaning the symbols
+            word = word.replace('.', '').replace('!', '').replace(',', ' ').replace('"', '').replace('?', '').replace(
+                ' ', '').replace("'s", '').lower()
 
-abCSV()
+            set.append(i + 1)       #Is set the words locations?
+            # getting the information of the word
+            if word in srtDictionary:
+                # srtDictionary[word][1] = the number of the instances of the word
+                set.append(srtDictionary[word][1] + 1)
+            else:
+                set.append(1)
+            srtDictionary[word] = set
+            set = []
+    # print (srtDictionary)
+    del (srtDictionary[''])
+    # print (srtDictionary)
+
+    # initilaing the names list with X| srt name is the speaker of the specific talker of the subtitle
+    subtitleSpeaker = []
+    for i in range(len(scriptText)+1):
+        subtitleSpeaker.append('X')
+
+    currentSpeaker = 'X'
+    # Run for all the word in the dictionary
+    for word in srtDictionary:
+        # Deal only with words with 1 instance
+        if srtDictionary[word][1] == 1:
+
+            # search the specific word in the script
+            for i in range(len(whatIsSaid)):
+                if (word in whatIsSaid[i]):
+                    # Finding the name of the speaker
+                    currentSpeaker = speakers[i]
+            # Put the name of the speaker in @srtName, srtDictionary[word][0] =the number of the subtitle
+            subNumber = srtDictionary[word][0]
+           #print(subNumber)
+            subtitleSpeaker[subNumber] = currentSpeaker
+            # print(word, srtDictionary[word],currentSpeaker)
+
+    with open(MovieName+' srt-script.csv', 'w',encoding='utf8') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(("Speaker", 'Begin time', 'End time', 'Text'))
+        for i in range(0, len(number) - 1):
+            writer.writerow((subtitleSpeaker[i], beginTime[i], endTime[i], scriptText[i]))
+
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-from LotkerFinal import Barak
+import Barak as b
+
+
+#This functions is making ab.csv table and srt-script file for each one of the movies
+b.makeABandScriptSrt('Dark Knight Rises')
+b.makeABandScriptSrt('Captain America - Civil War')
